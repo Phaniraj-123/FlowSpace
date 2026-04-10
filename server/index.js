17b9c54c66886b0dcbc1d924d0f286df18e4504e
Phanirajbn-123 <phanirajbn152@gmail.com>
2026-04-10 10:51:44 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -13,12 +13,12 @@ const httpServer = http.createServer(app)
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://flowspace-one.vercel.app'],
     credentials: true
   }
 })
 
-app.use(cors({ origin: ['http://localhost:3001', 'http://localhost:5174', 'http://localhost:5173'], credentials: true }))
+app.use(cors({ origin: ['http://localhost:3001', 'http://localhost:5174', 'http://localhost:5173', 'https://flowspace-one.vercel.app'], credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
